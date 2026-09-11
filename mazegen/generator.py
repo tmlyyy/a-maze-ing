8e7dd5043824e81ab7186871434d0b7aaf03df00
Guilherme Proenca Steffen <gproenca@c3r2p2.42sp.org.br>
2026-09-11 15:32:28 -0300

    gen

--- mazegen/generator.py
+++ mazegen/generator.py
@@ -13,14 +13,46 @@ DELTA = {NORTH: (O, -1), SOUTH: (O, 1), EAST: (1, 0) WEST: (-1, 0)}
 
 
 class MazeGenerator:
-    def__init__(self, width: int, height: int, entry: tuple[int, int],
+    def __init__(self, width: int, height: int, entry: tuple[int, int],
                 exit: tuple[int, int], seed: int = 0, perfect: bool = true):
-    validate_dimensions(width, height, entry, exit)
-    self.width = width
-    self.height = height
-    self.entry = entry
-    self.exit = exit
-    self.seed = seed
-    self.perfect = perfect
-    self._rng = random.Random(seed)
-    self.grid = None
+        validate_dimensions(width, height, entry, exit)
+        self.width = width
+        self.height = height
+        self.entry = entry
+        self.exit = exit
+        self.seed = seed
+        self.perfect = perfect
+        self._rng = random.Random(seed)
+        self.grid = None
+
+    def generate(self) -> None:
+        self.grid = self._init_grid()
+        self._carve_perfect_maze()
+        self._insert_42_pattern()
+        if not self.perfect:
+            sel._add_loops()
+	
+    def solve(self) -> list[str]:
+        if self.grid is None:
+            raise RuntimeError("Chame generate() antes de solve().")
+        return bfs_shortest_path(self.grid, self.entry, self.exit)
+
+    def get_structure(self) -> list[list[int]]:
+        return self.grid
+
+    def _init_grid(self):
+        grid = [[NORTH | EAST | SOUTH | WEST for _ in range(self.width)]
+                for _ in range(self.height)]
+		
+		for (x, y) in (self.entry, self.exit):
+			if x == 0:
+				grid[y][x] &= ~WEST
+			elif x == self.width - 1:
+				grid[y][x] &= ~EAST
+			if y == 0:
+				grid[y][x] &= ~NORTH
+			elif y == self.height -1:
+				grid[y][x] &= ~SOUTH
+
+		return grid
+        
